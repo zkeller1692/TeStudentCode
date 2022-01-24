aeb9f0fe8239675dcc5a1bb5d966b10fd25e2db4
Zach Keller <zkellerz68@gmail.com>
2022-01-24 18:48:51 -0500

    exercise complete

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -4,38 +4,37 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Please enter a number: ");
-		String numStr = scanner.nextLine();
-		int num = Integer.parseInt(numStr);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        String numStr = scanner.nextLine();
+        int num = Integer.parseInt(numStr);
 
-		int[] fibArray = new int[num];
-//		for (int i = 0; i >= num;i++){
-//			System.out.print(fibArray);
-//		}
-//		int[] testScores = new int[num];
-//
-//// ... array is populated
-//
-//		boolean hasPerfectScore = false;
-//
-//		for (int i = 0; i < testScores.length; i++) {
-//			if (testScores[i] == 100) {
-//				hasPerfectScore = true;
-//				break;      // stop looking for a perfect score if you've found one
-//				System.out.println(testScores);
-//			}
-//		}
-		int i = 0;
-		while (i < num);
+        if (num <= 0) {
+            System.out.println("0,1");
+        }
+        if (num == 1) {
+            System.out.println("0,1,1");
+        }
+        if (num >= 2)
+            System.out.println("0, 1, ");{
+            int current = 1;
+            int previous = 0;
+            int sum = 0;
+
+            while (sum < num) {
+                sum = current + previous;
+                previous = current;
+                current = sum;
+                if (sum > num) {
+                    break;
+                }
+                System.out.format("%d, ", sum);
+            }
+        }
 
 
-
-
-
-	}
-
+    }
 
 
 }
--- module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Intro_to_Objects_Strings/lecture/src/main/java/com/techelevator/Lecture.java
@@ -2,70 +2,99 @@ package com.techelevator;
 
 public class Lecture {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("************************************");
-		System.out.println("****** MAKING A STRING OBJECT ******");
-		System.out.println("************************************");
+        System.out.println("************************************");
+        System.out.println("****** MAKING A STRING OBJECT ******");
+        System.out.println("************************************");
 
-		/* The String class gets special treatment in the Java language.  One
-		 * example of this is that there is a literal representation of a
-		 * String (i.e. characters appearing between two double quotes.  This
-		 * is not the case for most classes */
+        /* The String class gets special treatment in the Java language.  One
+         * example of this is that there is a literal representation of a
+         * String (i.e. characters appearing between two double quotes.)  This
+         * is not the case for most classes */
 
-		/* create an new instance of String using a literal */
+        /* create an new instance of String using a literal */
+        int x = 5;
+        String name = "Dave";
+        String salutation = new String(new char[]{'H', 'e', 'l', 'l', 'o'});
+        String greeting = null;
+        greeting = "hi";
 
-		
-		System.out.println();
-		System.out.println("******************************");
-		System.out.println("****** MEMBER METHODS ******");
-		System.out.println("******************************");
-		System.out.println();
+        System.out.println();
+        System.out.println("******************************");
+        System.out.println("****** MEMBER METHODS ******");
+        System.out.println("******************************");
+        System.out.println();
 
 
-		/* Other commonly used methods:
-		 *
-		 * endsWith
-		 * startsWith
-		 * indexOf
-		 * lastIndexOf
-		 * length
-		 * substring
-		 * toLowerCase
-		 * toUpperCase
-		 * trim
-		 */
+        /* Other commonly used methods:
+         *
+         * endsWith
+         * startsWith
+         * indexOf
+         * lastIndexOf
+         * length
+         * substring
+         * toLowerCase
+         * toUpperCase
+         * trim
+         */
 
-		System.out.println();
-		System.out.println("**********************");
-		System.out.println("****** EQUALITY ******");
-		System.out.println("**********************");
-		System.out.println();
+        String lectureString = "This is a string for Monday";
+        int length = lectureString.length();
+        System.out.println(length);
 
-        char[] helloArray = new char[] { 'H', 'e', 'l', 'l', 'o' };
+        String firstFive = lectureString.substring(0, 5);
+        System.out.println("firstFive= [" + firstFive + "]");
+        lectureString = lectureString.toUpperCase();
+        System.out.println(lectureString);
+
+        System.out.println();
+        System.out.println("**********************");
+        System.out.println("****** EQUALITY ******");
+        System.out.println("**********************");
+        System.out.println();
+
+        char[] helloArray = new char[]{'H', 'e', 'l', 'l', 'o'};
         String hello1 = new String(helloArray);
         String hello2 = new String(helloArray);
+        String concatStr = "Hello";
+        //concatStr = concatStr.concat(" goodbye");
+        concatStr = concatStr + " Goodbye";
+        concatStr = concatStr + 5;
 
-		/* Double equals will compare to see if the two variables, hello1 and
-		 * hello2 point to the same object in memory. Are they the same object? */
-		if (hello1 == hello2) {
-			System.out.println("They are equal!");
-		} else {
-			System.out.println(hello1 + " is not equal to " + hello2);
-		}
+        System.out.println(concatStr);
+        concatStr = "" + 5 + concatStr;
+        for (int i = 0; i < concatStr.length(); i++) {
+            System.out.println(concatStr.charAt(i));
+        }
 
-		String hello3 = hello1;
-		if (hello1 == hello3) {
-			System.out.println("hello1 is the same reference as hello3");
-		}
+        int indexOfIs = lectureString.indexOf("IS");
+        System.out.println("indexOfIs= " + indexOfIs);
+        int indexOfSecondIs = lectureString.indexOf("Is", 3);
+        System.out.println(indexOfSecondIs);
 
-		/* So, to compare the values of two objects, we need to use the equals method.
-		 * Every object type has an equals method */
-		if (hello1.equals(hello2)) {
-			System.out.println("They are equal!");
-		} else {
-			System.out.println(hello1 + " is not equal to " + hello2);
-		}
 
-	}
+        /* Double equals will compare to see if the two variables, hello1 and
+         * hello2 point to the same object in memory. Are they the same object? */
+        if (hello1 == hello2) {
+            System.out.println("They are equal!");
+        } else {
+            System.out.println(hello1 + " is not equal to " + hello2);
+        }
+
+        String hello3 = hello1;
+        if (hello1 == hello3) {
+            System.out.println("hello1 is the same reference as hello3");
+        }
+
+        /* So, to compare the values of two objects, we need to use the equals method.
+         * Every object type has an equals method */
+        if (hello1.equals(hello2)) {
+            System.out.println("They are equal!");
+        } else {
+            System.out.println(hello1 + " is not equal to " + hello2);
+        }
+
+    }
 }
