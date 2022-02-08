27cbafb4ce80a213a2da13cefa7fb1595cbcf960
Zach Keller <zkellerz68@gmail.com>
2022-02-08 18:32:13 -0500

    Exercise

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,35 @@
 package com.techelevator;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replaceWord = userInput.nextLine();
+
+        System.out.println("What is the Source file?");
+        String sourceFile = userInput.nextLine();
+        File inputFile = new File(sourceFile);
+
+        System.out.println("What is the destination file?");
+        String destinationFile = userInput.nextLine();
+
+        try (Scanner inputFileScan = new Scanner(inputFile);
+        PrintWriter outputFile = new PrintWriter(destinationFile)){
+            while(inputFileScan.hasNextLine()){
+                String lineOfText = inputFileScan.nextLine();
+                outputFile.println(lineOfText.replaceAll(searchWord, replaceWord));
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
 
     }
-
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,34 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What is the destination file?");
+		String destinationFile = userInput.nextLine();
+		int count = 0;
+
+		try (PrintWriter outputFile = new PrintWriter(destinationFile)){
+			do{count++;
+				if(count %3 == 0 && count % 5 ==0){
+				outputFile.println("FizzBuzz");
+			}
+				else if (count % 3 == 0){
+					outputFile.println("Fizz");
+				}
+				else if (count % 5 == 0){
+					outputFile.println("Buzz");
+				}
+				else{ outputFile.println(count);}
+			}while(count <300);
+		}catch (FileNotFoundException e){
+			System.out.println("File not found");
+		}
 	}
 
 }
