19e1b4771ec1fd06600d303c2c999f615ea0cccf
Zach Keller <zkellerz68@gmail.com>
2022-02-07 20:55:12 -0500

    Exercise Complete

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,59 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
-	public static void main(String[] args) {
 
-	}
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What is the fully qualified name of the file that should be searched?");
+        String filePath = userInput.nextLine();
+        File inputFile = new File(filePath);
+        int lineCount = 0;
 
+        System.out.println("What is the search word you are looking for?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("Should the search be case sensitive?");
+        String caseSense = userInput.nextLine();
+
+        if (caseSense.equalsIgnoreCase("y")) {
+            try (Scanner fileInput = new Scanner(inputFile)) {
+                while (fileInput.hasNextLine()) {
+                    String lineOfText = fileInput.nextLine();
+                    lineCount++;
+                    if (!lineOfText.contains(searchWord)) {
+                        continue;
+                    }
+                    System.out.println(lineCount + ") " + lineOfText);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+            }
+        }
+        if (caseSense.equalsIgnoreCase("n")){
+            try (Scanner fileInput = new Scanner(inputFile)) {
+                while (fileInput.hasNextLine()) {
+                    String lineOfText = fileInput.nextLine();
+                    lineCount++;
+                    if (!lineOfText.toLowerCase().contains(searchWord.toLowerCase())) {
+                        continue;
+                    }
+                    System.out.println(lineCount + ") " + lineOfText);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+            }
+        }
+
+
+
+
+
+
+
+    }
 }
--- module-1/16b_File_IO_Reading/lecture/src/main/java/com/techelevator/scanner/RTNValidator.java
+++ module-1/16b_File_IO_Reading/lecture/src/main/java/com/techelevator/scanner/RTNValidator.java
@@ -5,56 +5,74 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class RTNValidator {
-	
-	private static final int[] CHECKSUM_WEIGHTS = new int[] { 3, 7, 1, 3, 7, 1, 3, 7, 1 };
 
-	public static void main(String[] args) throws FileNotFoundException {
+    private static final int[] CHECKSUM_WEIGHTS = new int[]{3, 7, 1, 3, 7, 1, 3, 7, 1};
 
-		printApplicationBanner();
-		
-		File inputFile = getInputFileFromUser();
-		try(Scanner fileScanner = new Scanner(inputFile)) {
-			while(fileScanner.hasNextLine()) {
-				String line = fileScanner.nextLine();
-				String rtn = line.substring(0, 9);
-				
-				if(checksumIsValid(rtn) == false) {
-					System.out.println(line);
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws FileNotFoundException {
 
-	private static void printApplicationBanner() {
-		System.out.println("******************");
-		System.out.println("RTN Validator 9000");
-		System.out.println("******************");
-		System.out.println();
-	}
+        printApplicationBanner();
 
-	@SuppressWarnings("resource")
-	private static File getInputFileFromUser() {
-		Scanner userInput = new Scanner(System.in);
-		System.out.print("Please enter path to input file >>> ");
-		String path = userInput.nextLine();
-		
-		File inputFile = new File(path);
-		if(inputFile.exists() == false) { // checks for the existence of a file
-			System.out.println(path+" does not exist");
-			System.exit(1); // Ends the program
-		} else if(inputFile.isFile() == false) {
-			System.out.println(path+" is not a file");
-			System.exit(1); // Ends the program
-		}
-		return inputFile;
-	}
+        File inputFile = getInputFileFromUser();
+        try (Scanner fileScanner = new Scanner(inputFile)) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (line.length() < 9) {
+                    System.out.println(line);
+                } else {
+                    String rtn = line.substring(0, 9);
 
-	private static boolean checksumIsValid(String routingNumber) {
-		int checksum = 0;
-		for(int i = 0; i < 9; i++) {
-			int digit = Integer.parseInt(routingNumber.substring(i, i+1));
-			checksum += digit * CHECKSUM_WEIGHTS[i];
-		}
-		return checksum % 10 == 0;
-	}
+                    if (checksumIsValid(rtn) == false) {
+                        System.out.println(line);
+                    }
+                }
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
+        String quizQuestion = "What is 2 + 2? |4*|2|9|who knows";
+
+        String [] quizParts = quizQuestion.split("\\|");
+
+        for (String quizPart : quizParts){
+            System.out.println(quizPart);
+        }
+    }
+
+    private static void printApplicationBanner() {
+        System.out.println("******************");
+        System.out.println("RTN Validator 9000");
+        System.out.println("******************");
+        System.out.println();
+    }
+
+    @SuppressWarnings("resource")
+    private static File getInputFileFromUser() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Please enter path to input file >>> ");
+        String path = userInput.nextLine();
+
+        File inputFile = new File(path);
+//        if (inputFile.exists() == false) { // checks for the existence of a file
+//            System.out.println(path + " does not exist");
+//            System.exit(1); // Ends the program
+//        } else if (inputFile.isFile() == false) {
+//            System.out.println(path + " is not a file");
+//            System.exit(1); // Ends the program
+//        }
+        return inputFile;
+    }
+
+    private static boolean checksumIsValid(String routingNumber) {
+        try {
+            int checksum = 0;
+            for (int i = 0; i < 9; i++) {
+                int digit = Integer.parseInt(routingNumber.substring(i, i + 1));
+                checksum += digit * CHECKSUM_WEIGHTS[i];
+            }
+            return checksum % 10 == 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
