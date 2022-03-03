e8f41281815d0a80607517c50a6a93ae6cd58694
Zach Keller <zkellerz68@gmail.com>
2022-03-03 01:55:43 -0500

    Exercise complete

--- module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,12 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientResponseException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -12,6 +17,30 @@ public class AuctionController {
 
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam(defaultValue = "") String title_like,
+                              @RequestParam(defaultValue = "0") double currentBid_lte) {
+        List<Auction> auctions = this.dao.list();
+        if (!title_like.equals("") && currentBid_lte > 0) {
+            return this.dao.searchByTitleAndPrice(title_like, currentBid_lte);
+        } else if (currentBid_lte > 0 && title_like.equals("")) {
+            return dao.searchByPrice(currentBid_lte);
+        } else if (!title_like.equals("")) {
+            return this.dao.searchByTitle(title_like);
+        }
+        return auctions;
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+        return this.dao.get(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction newAuction) {
+        return this.dao.create(newAuction);
     }
 
 }
--- module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
+++ module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -5,10 +5,12 @@ import com.techelevator.reservations.dao.MemoryHotelDao;
 import com.techelevator.reservations.dao.MemoryReservationDao;
 import com.techelevator.reservations.dao.ReservationDao;
 import com.techelevator.reservations.model.Hotel;
+import com.techelevator.reservations.model.Reservation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class HotelController {
 
     private HotelDao hotelDao;
@@ -40,4 +42,23 @@ public class HotelController {
         return hotelDao.get(id);
     }
 
+    @GetMapping("/reservations")
+    public List<Reservation> getReservations(){
+        return this.reservationDao.findAll();
+    }
+
+    @GetMapping("/reservation/{id}")
+    public Reservation getReservationById(@PathVariable int id){
+        return this.reservationDao.get(id);
+    }
+
+    @GetMapping("hotels/{id}/reservations")
+    public List<Reservation> getReservationsByHotelId(@PathVariable int hotelId){
+        return this.reservationDao.findByHotel(hotelId);
+    }
+
+    @PostMapping("/reservation")
+    public Reservation createdReservation(@RequestBody Reservation newReservation){
+        return this.reservationDao.create(newReservation, newReservation.getHotelID());
+    }
 }
