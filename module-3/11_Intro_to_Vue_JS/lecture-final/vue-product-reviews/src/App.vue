91f2b158019fa3c634bdbefa267dc0e3f0113a35
Dave Hoy <david.hoy@techelevator.com>
2022-03-28 14:14:34 -0400

    M3D11 Lecture final

---
+++ module-3/11_Intro_to_Vue_JS/lecture-final/vue-product-reviews/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <product-review/>
+    <hello-world msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import ProductReview from '@/components/ProductReview.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ProductReview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+    max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
