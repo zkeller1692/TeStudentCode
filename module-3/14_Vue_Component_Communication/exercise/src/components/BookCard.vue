7c0ae831c20c9fb84a81fc4dda9f768a7cb7f704
Zach Keller <zkellerz68@gmail.com>
2022-04-04 20:47:50 -0400

    Exercise complete

--- module-3/14_Vue_Component_Communication/exercise/src/components/BookCard.vue
+++ module-3/14_Vue_Component_Communication/exercise/src/components/BookCard.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="card">
     <h2 class="book-title">{{ book.title }}</h2>
-    <img
-      v-if="book.isbn"
-      v-bind:src="
-        'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
-      "
-    />
+    <img v-if="book.isbn" v-bind:src=" 'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'"/>
     <h3 class="book-author">{{ book.author }}</h3>
-    <button
-      class="mark-read"
-      v-if="book.read == false"
-      v-on:click="onReadUnreadChange"
-    >
+    <button class="mark-read" v-if="book.read == false" v-on:click="onReadUnreadChange">
       {{ book.read === true ? "Mark Unread" : "Mark Read" }}
     </button>
-    <button
-      class="mark-unread"
-      v-if="book.read == true"
-      v-on:click="onReadUnreadChange"
-    >
+    <button class="mark-unread" v-if="book.read == true" v-on:click="onReadUnreadChange">
       {{ book.read === true ? "Mark Unread" : "Mark Read" }}
     </button>
   </div>
